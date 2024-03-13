50f3db82cfa1c8c370d157c6fe775af73c6b7683
Nandini <nandinijindal010@gmail.com>
2024-03-13 08:17:23 +0530

    cycle,kahns

--- Nandini/Leetcode/Graph/GoodQues/CycleInDirected.cpp
+++ Nandini/Leetcode/Graph/GoodQues/CycleInDirected.cpp
@@ -38,3 +38,41 @@ class Solution {
         return false;
     }
 };
+
+class TropoSort {
+  public:
+    bool isCyclic(int n, vector<int> adj[]) 
+    {
+        queue<int> q;
+	    vector<int> indegree(n,0);
+	    vector<int> ans;
+	    for(int i=0 ; i<n ; i++)
+	    {
+	        for(auto it : adj[i])
+	        {
+	            indegree[it]++;
+	        }
+	    }
+	    for(int i=0 ; i <n ; i++)
+	    {
+	        if(indegree[i]==0)
+	        {
+	            q.push(i);
+	        }
+	    }
+	    
+	    while(!q.empty())
+	    {
+	        int node = q.front(); q.pop();
+	        ans.push_back(node);
+	        for(auto it : adj[node])
+	        {
+	            indegree[it]--;
+	            if(indegree[it]==0)q.push(it);
+	        }
+	    }
+	    
+	    if(ans.size()==n) return false;
+	    return true;
+    }
+};
