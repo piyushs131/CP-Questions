f96c05a294d9706f01f82ddd08ba24081fba4d94
Nandini Jindal <nandinijindal010@gmail.com>
2024-04-09 08:18:45 +0530

    fiibo

--- Nandini/Tocode.cpp
+++ Nandini/Tocode.cpp
@@ -1,30 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
-class Solution 
-{
-    public :
-    string findString(string s)
-    {
-        string ans = s;
-        if(s[0]>=97 && s[0]<=122)
-        s[0] = s[0]-32;
-        else 
-        s[0]+=32;
-
-        for(int i=1 ; i<s.size() ; i++)
-        {
-            if(s[i]>=97 && s[i]<=122)
-            return ans;
-
-            s[i] = s[i]+32;
-        }
-        return s;
-    }
-};
 int main()
 {
-    string s;
-    cin >> s ;
-    Solution obj;
-    cout << obj.findString(s);
+    int n,a;
+    cin >> n;
+    vector<int> v;
+    for(int i=0 ; i<n ; i++)
+    {
+        cin >> a;
+        v.push_back(a);
+    }
+    sort(v.begin(),v.end());
+    
 }
