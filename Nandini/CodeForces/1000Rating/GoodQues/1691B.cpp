43ff3f9468abffa7543a1dfa71061cf1a7d19772
Nandini Jindal <nandinijindal010@gmail.com>
2024-04-13 07:41:41 +0530

    1691B

--- Nandini/CodeForces/1000Rating/GoodQues/1691B.cpp
+++ Nandini/CodeForces/1000Rating/GoodQues/1691B.cpp
@@ -1,45 +1,44 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-#define ll long long
+ 
 int main()
 {
-    ll t;
-    cin >> t;
-    while (t--)
-    {
-        ll n;
-        cin >> n;
-        unordered_map<int,queue<int> > freq;
-        vector<int> v(n),ans(n);
-        bool valid = true;
-        for(int i=0 ; i<n ; i++)
-        {
-            cin >> v[i];
-            freq[v[i]].push(i);
-        }
-        for(int i=0 ; i<n ; i++)
-        {
-            if(freq[v[i]].size()<2)
-            {valid = false;break;}
-        }
-        if(!valid)
-        {
-            cout << -1 << endl;
-        }
-        else 
-        {
-            for(auto e : freq)
-            {
-                int val = freq[e.first].front();
-                freq[e.first].pop();
-                freq[e.first].push(val);
-            }
-            for(int i=0 ;i <n ; i++)
-            {
-               cout << freq[v[i]].front()+1 << " ";
-               freq[v[i]].pop();
-            }
-            cout << endl;
-        }
-    }
-}
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		long long a[100005],b[100005];
+		cin>>n;
+		for(int i=0;i<n;i++)
+		{
+			cin>>a[i];
+			b[i]=i+1;
+		}
+		int cnt=1,flag=0;
+		for(int i=1;i<n;i++)
+		{
+			while(a[i]==a[i-1]&&i<n)
+			{
+				cnt++;
+				swap(b[i],b[i-1]); 
+				i++;
+			 } 
+			 if(cnt<=1)
+			 {
+			 	flag=1;
+			 	break;
+			 }
+			 else
+			 cnt=1; 
+		}
+		if(a[n-1]!=a[n-2])
+		flag=1;
+		if(flag==1)
+		cout<<"-1";
+		else
+		for(int i=0;i<n;i++)
+		cout<<b[i]<<" ";
+		cout<<endl;
+	}
+ }
--- Nandini/Tocode.cpp
+++ Nandini/Tocode.cpp
@@ -1,15 +1,45 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+#define ll long long
 int main()
 {
-    int a,b;
-    cin >> a >> b;
-    int sum=a;
-    int n = a;
-    while(n>b)
+    ll t;
+    cin >> t;
+    while (t--)
     {
-        sum+= n/b;
-        n = n/b + n%b;
+        ll n;
+        cin >> n;
+        unordered_map<int,queue<int> > freq;
+        vector<int> v(n),ans(n);
+        bool valid = true;
+        for(int i=0 ; i<n ; i++)
+        {
+            cin >> v[i];
+            freq[v[i]].push(i);
+        }
+        for(int i=0 ; i<n ; i++)
+        {
+            if(freq[v[i]].size()<2)
+            {valid = false;break;}
+        }
+        if(!valid)
+        {
+            cout << -1 << endl;
+        }
+        else 
+        {
+            for(auto e : freq)
+            {
+                int val = freq[e.first].front();
+                freq[e.first].pop();
+                freq[e.first].push(val);
+            }
+            for(int i=0 ;i <n ; i++)
+            {
+               cout << freq[v[i]].front()+1 << " ";
+               freq[v[i]].pop();
+            }
+            cout << endl;
+        }
     }
-    cout << sum;
 }
