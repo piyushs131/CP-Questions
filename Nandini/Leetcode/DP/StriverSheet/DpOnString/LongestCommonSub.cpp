9effc6a97ad37180afeae4ceefb2ce2927af637d
Nandini Jindal <nandinijindal010@gmail.com>
2024-04-23 07:49:36 +0530

    longest common sub

--- Nandini/Leetcode/DP/StriverSheet/DpOnString/LongestCommonSub.cpp
+++ Nandini/Leetcode/DP/StriverSheet/DpOnString/LongestCommonSub.cpp
@@ -1,5 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
+class Tabulation {
+public:
+    int longestCommonSubsequence(string s, string t) 
+    {
+       int n1 = s.size(),n2 = t.size();
+        vector<vector<int> > dp(n1+1,vector<int> (n2+1,-1));
+       for(int i=0 ;i <=n1 ; i++)
+           dp[i][0] = 0;
+        
+        for(int j=0 ;j <=n2 ; j++)
+           dp[0][j] = 0;
+        
+        for(int i=1 ;i<=n1 ; i++)
+        {
+            for(int j=1 ; j<=n2 ; j++)
+            {
+                if(s[i-1]==t[j-1])  dp[i][j] = 1+ dp[i-1][j-1];
+
+                else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        
+        return dp[n1][n2];
+    }
+};
 int longestSub(string s,string t,int i,int j,vector<vector<int> > &dp)
 {
    if(i<0 || j<0) return 0;
