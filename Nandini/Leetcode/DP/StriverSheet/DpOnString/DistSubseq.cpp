a860b91e050b587a8d93facc10765865a8892aee
Nandini Jindal <nandinijindal010@gmail.com>
2024-04-25 08:24:20 +0530

    edit distance

--- Nandini/Leetcode/DP/StriverSheet/DpOnString/DistSubseq.cpp
+++ Nandini/Leetcode/DP/StriverSheet/DpOnString/DistSubseq.cpp
@@ -22,3 +22,26 @@ int distinctSubsequences(string &s, string &t)
     vector<vector<int>> dp(n1, vector<int>(n2, -1));
     return ways(n1 - 1, n2 - 1, s, t, dp);
 }
+
+class Solution {
+public:
+    int numDistinct(string s, string t) 
+    {
+       int n1 = s.size(),n2 = t.size();
+       vector<vector<double> > dp(n1+1,vector<double> (n2+1,0)); 
+       for(int i=0 ; i<=n1 ; i++) dp[i][0] = 1;
+        
+        for(int i=1 ; i<=n1 ; i++)
+        {
+            for(int j=1; j<=n2 ; j++)
+            {
+                if(s[i-1]==t[j-1])
+                    dp[i][j]= dp[i-1][j-1] + dp[i-1][j];
+                else 
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+        
+        return (int)dp[n1][n2];
+    }
+};
