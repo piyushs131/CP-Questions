d7394c1c98cfd14b9ea6e192256f262297154457
Nandini Jindal <nandinijindal010@gmail.com>
2024-04-29 07:58:31 +0530

    buy or sell

--- Nandini/Leetcode/DP/StriverSheet/SubseqProblems/WildCardPatternMatching.cpp
+++ Nandini/Leetcode/DP/StriverSheet/SubseqProblems/WildCardPatternMatching.cpp
@@ -1,5 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
+class Solution {
+public:
+    bool isMatch(string b, string a) 
+    {
+       int n = a.size(),m = b.size();
+        bool flag = true;
+        vector<vector<bool> > dp(n+1,vector<bool> (m+1,false));
+        dp[0][0] = true;
+        for(int j=1 ; j<=m ; j++) dp[0][j] = false;
+        for(int i=1 ; i<=n ; i++)
+        {
+            flag = true;
+            for(int k=1 ; k<=i ; k++)
+            {
+                if(a[k-1]!='*') {flag= false;
+                                break;}
+            }
+            dp[i][0] = flag;
+        }
+        
+        for(int i=1 ; i<=n ; i++)
+        {
+            for(int j=1 ; j<=m ; j++)
+            {
+                if(a[i-1]==b[j-1] || a[i-1]=='?') dp[i][j] = dp[i-1][j-1];
+                
+                else if(a[i-1]=='*') dp[i][j] = dp[i-1][j] | dp[i][j-1];
+                else dp[i][j] = false;
+            }
+        }
+        
+        return dp[n][m];
+    }
+};
 bool f(int i,int j,string a,string b,vector<vector<int> > &dp)
 {
    if(i<0 && j<0) return true;
