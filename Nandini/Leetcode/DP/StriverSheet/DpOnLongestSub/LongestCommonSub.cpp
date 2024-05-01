5bdc3d3a1e69113c0a33992e770d176edad7a070
Nandini Jindal <nandinijindal010@gmail.com>
2024-05-01 16:45:05 +0530

    print longest common  sub

--- Nandini/Leetcode/DP/StriverSheet/DpOnLongestSub/LongestCommonSub.cpp
+++ Nandini/Leetcode/DP/StriverSheet/DpOnLongestSub/LongestCommonSub.cpp
@@ -16,3 +16,45 @@ int longestIncreasingSubsequence(int arr[], int n)
     vector<vector<int> > dp(n,vector<int>(n+1,-1));
     return f(0,-1,arr,n,dp);
 }
+int longestIncreasingSubsequence2_spaceOptimised(int arr[], int n)
+{
+    vector<int> next(n+1,0),curr(n+1,0);
+    int len;
+    int prev;
+    for(int ind=n-1 ; ind>=0 ; ind--)
+    {
+        for( prev = ind-1 ; prev>=-1 ; prev--)
+        {
+            //                                +1 because can't be -1
+              len = 0+next[prev+1];
+             if(prev==-1 || arr[ind]>arr[prev])
+             len = max(len,1+next[ind+1]);
+
+             curr[prev+1] = len;
+        }
+        next = curr;
+    }
+    return next[-1+1];
+}
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& arr) 
+    {
+        int n = arr.size();
+        vector<int> dp(n,1);
+    int maxi = 1;
+    for(int i=0 ; i<n ; i++)
+    {
+        for(int prev = 0 ; prev<i ; prev++)
+        {
+            if(arr[prev]<arr[i])
+            {
+                dp[i] = max(dp[i],1+dp[prev]);
+            }
+        }
+
+        maxi = max(maxi,dp[i]);
+    }
+    return maxi;
+    }
+};
