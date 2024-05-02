45b752b174376f4107eb4d7bf9da8653248525bb
Nandini Jindal <nandinijindal010@gmail.com>
2024-05-02 16:27:36 +0530

    longest common sub

--- Nandini/Leetcode/DP/StriverSheet/DpOnLongestSub/LongestCommonSub.cpp
+++ Nandini/Leetcode/DP/StriverSheet/DpOnLongestSub/LongestCommonSub.cpp
@@ -58,3 +58,20 @@ public:
     return maxi;
     }
 };
+
+int longestIncreasingSubsequence_best(int arr[], int n)
+{
+    vector<int> temp;
+    temp.push_back(arr[0]);
+    for(int i=1 ; i<n ; i++)
+    {
+        if(arr[i]>temp.back())
+        temp.push_back(arr[i]);
+        else 
+        {
+             auto it = lower_bound(temp.begin(), temp.end(), arr[i]);
+            *it = arr[i];
+        }
+    }
+    return temp.size();
+}
