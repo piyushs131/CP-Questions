931e7243c975281477c8391c562f94bc22b428d5
Nandini Jindal <nandinijindal010@gmail.com>
2024-05-05 09:43:56 +0530

    matrix chain mul tabulation

--- Nandini/Leetcode/DP/StriverSheet/PartitionDP/matrixChainMul.cpp
+++ Nandini/Leetcode/DP/StriverSheet/PartitionDP/matrixChainMul.cpp
@@ -18,3 +18,23 @@ int matrixMultiplication(vector<int> &arr, int n)
     vector<vector<int>> dp(n, vector<int>(n, -1));
     return f(1,n-1,arr,dp);
 }
+
+int matrixMultiplication_Tabulation(vector<int> &arr, int n)
+{
+    vector<vector<int>> dp(n, vector<int>(n, 0));
+   for(int i=n-1 ; i>=1 ; i--)
+   {
+      for(int j=i+1 ; j<n ; j++)
+      {
+          int mini = 1e9;
+         for(int k=i ; k<j; k++)
+         {
+        int steps = arr[i-1]*arr[k]*arr[j] + dp[i][k] + dp[k+1][j];
+        if(steps<mini) mini = steps;
+         }
+         dp[i][j] = mini;
+      }
+   }
+
+   return dp[1][n-1];
+}
