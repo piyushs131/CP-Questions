52dca0607b63bed8c780ce107663b8b8a29da739
Nandini Jindal <nandinijindal010@gmail.com>
2024-06-21 10:58:45 +0530

    Dijstras with set and pq

--- Nandini/Leetcode/Graph/StriverSheet/ShortestPath/DijkstraAlgo.cpp
+++ Nandini/Leetcode/Graph/StriverSheet/ShortestPath/DijkstraAlgo.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-class Solution
+
+class SolutionWithPq
 {
 	public:
     vector <int> dijkstra(int n, vector<vector<int>> adj[], int s)
@@ -22,6 +23,41 @@ class Solution
                 {
                     dist[it[0]] = dis+it[1];
                     pq.push({dis+it[1],it[0]});
+                }
+            }
+        }
+
+        return dist;
+    }
+};
+//better
+class SolutionWithSet
+{
+	public:
+    vector <int> dijkstra(int n, vector<vector<int>> adj[], int s)
+    {
+        vector<int > ans;
+        //min heap
+        set<pair<int,int> > st;
+        vector<int> dist(n,1e9);
+        dist[s] = 0;
+        st.insert({0,s});
+        while(!st.empty())
+        {
+            auto it  = *(st.begin());
+            int node = it.second;
+            int dis= it.first;
+            st.erase(it);
+            for(auto it : adj[node])
+            {
+                if(dist[it[0]] > dis+ it[1])
+                {
+                    if(dist[it[0]]!=1e9)
+                    {
+                        st.erase({dist[it[0]],it[1]});
+                    }
+                    dist[it[0]] = dis+it[1];
+                    st.insert({dis+it[1],it[0]});
                 }
             }
         }
