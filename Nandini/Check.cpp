edcccf14d9b792f8b2657e01857a300f490374de
Nandini Jindal <nandinijindal010@gmail.com>
2024-07-10 09:58:54 +0530

    Li hua and pattern

--- Nandini/Check.cpp
+++ Nandini/Check.cpp
@@ -3,30 +3,53 @@ using namespace std;
 #define ll long long int
 
 void solve() {
-    ll n;
-    cin >> n;
-    vector<ll> arr(n);
+    int n, k;
+    cin >> n >> k;
+    vector<vector<int>> grid(n, vector<int>(n, 0));
     
-    for(ll i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
-
-    priority_queue<ll> pq;
-    ll power = 0;
-    
-    for(ll i = 0; i < n; i++) {
-        if(arr[i] != 0) 
-        {
-            pq.push(arr[i]);
-        } 
-        else if(arr[i] == 0 && !pq.empty()) 
-        {
-            power += pq.top();
-            pq.pop();
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> grid[i][j];
         }
     }
+    if(n==1)
+    {
+        cout << "YES" << endl;
+        return;
+    }
     
-    cout << power << endl;
+    int count = k;
+    bool f = true;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (grid[i][j] != grid[n - i - 1][n - j - 1]) 
+            { 
+               if(count==0)
+               {
+                 f = false;
+                 break;
+               }  
+               count--;
+               grid[i][j] = grid[n - i - 1][n - j - 1];
+            }
+        }
+        if(f==false)  break;
+    }
+
+    int ct =0;
+    for(int i=0 ; i<n ; i++)
+    {
+        for(int j=0 ; j<n ;j ++)
+        {
+            if(grid[i][j]!=grid[n-i-1][n-j-1])
+            ct++;
+        }
+    }
+    if(ct!=0) cout << "NO" << endl;
+    else {
+        if(count%2==0) cout << "YES" << endl;
+        else cout << "NO" << endl;
+    }
 }
 
 int main() {
