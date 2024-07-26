926561478b57b139909608e8138a72ed83676869
Nandini Jindal <nandinijindal010@gmail.com>
2024-07-26 20:37:05 +0530

    scale

--- Nandini/Check.cpp
+++ Nandini/Check.cpp
@@ -3,31 +3,28 @@ using namespace std;
 #define ll long long
 void solve()
 {
-	int n,k;
-	cin >> n >> k;
-
-    vector<vector<int> > v(n,vector<int>(n,0));
-	for(int i=0 ;i <n ; i++)
+	int n,q;
+	cin >> n >> q;
+	string a ,b;
+	cin >> a >> b;
+	while(q--)
 	{
-		string c;
-		cin >> c;
-		int j=0;
-		for(int h=0 ; h<c.size() ; h++)
-		v[i][j++] = c[h]-'0';
-
-
-	}
-    int i=0;
-	while(i<n)
-	{
-        int j=0; 
-		while(j<n)
+		int a1,a2;
+		cin >> a1 >> a2;
+		a1--,a2--;
+		map<int,int> mp;
+		int ans =0;
+		for(int i=a1 ; i<=a2 ; i++)
 		{
-			cout << v[i][j] ;
-			j+=k;
+			mp[a[i]-'0'-49]++;
+			mp[b[i]-'0'-49]--;
 		}
-		cout << endl;
-		i+=k;
+		for(auto it : mp)
+		{
+			// cout << it.first << " " << it.second << endl;
+			if(it.second!=0) ans++;
+		}
+		cout << ans/2 << endl;
 	}
 
 }
