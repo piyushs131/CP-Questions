acaf8c9675a3710b9420d8bb031b106467b9d30d
Nandini Jindal <nandinijindal010@gmail.com>
2024-08-04 10:42:59 +0530

    freq of the most frequent element

---
+++ Nandini/PracticeQuestions/Greedy/FreqofMostFrequent.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define lli long long int
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int left = 0;
+        int ans = 0;
+        long curr = 0;
+        
+        for (int right = 0; right < nums.size(); right++) {
+            long target = nums[right];
+            curr += target;
+            
+            while ((right - left + 1) * target - curr > k) {
+                curr -= nums[left];
+                left++;
+            }
+            
+            ans = max(ans, right - left + 1);
+        }
+        
+        return ans;
+    }
+};
