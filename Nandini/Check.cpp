0be3c70c9260a6bb39d66241f90191a6762ac450
Nandini Jindal <nandinijindal010@gmail.com>
2024-08-06 20:28:57 +0530

    adding

--- Nandini/Check.cpp
+++ Nandini/Check.cpp
@@ -1,9 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define lli long long int
+#define ll long long int
 void solve()
 {
-     
+   int a1,a2,b1,b2;
+   cin >> a1 >> a2 >> b1 >> b2;
+   int count1 =0;
+   if(a1>b1) count1++;
+   if(a1>b2) count1++;
+   if(a2>b1) count1++;
+   if(a2>b2) count1++;
+
+   if(count1==3)
+   {
+    cout << 2 << endl;
+   }
+   else if(count1==1 || count1==2 || count1==0) {
+    cout << 0 << endl;
+   }
+   else cout << 4 << endl;
 }
 int main() 
 {
