e0b286f9d8cabc7ee9c6726d54579e7a2c55aada
Nandini Jindal <nandinijindal010@gmail.com>
2024-08-07 07:15:38 +0530

    max consecutive 1's best soln

--- Nandini/Striver_SDE_Sheet/TwoPointer/MaxConsecutiveOnes.cpp
+++ Nandini/Striver_SDE_Sheet/TwoPointer/MaxConsecutiveOnes.cpp
@@ -23,3 +23,29 @@ public:
        return ans; 
     }
 };
+
+class Solution2 {
+public:
+    int longestOnes(vector<int>& nums, int k) 
+    {
+       int n = nums.size();
+       int l =0, r=0,zeros=0, ans =0;
+       while(l<=r && r<n)
+       {
+         if(nums[r]==0)
+         zeros++;
+
+         if(zeros>k)
+         {
+            if(nums[l]==0) zeros--;
+            l++;
+         }
+         if(zeros<=k)
+         {
+            ans = max(ans,r-l+1);
+         }
+         r++;
+       }
+       return ans; 
+    }
+};
