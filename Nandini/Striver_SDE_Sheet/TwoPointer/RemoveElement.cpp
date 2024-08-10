a8b24a6a7a82f0d4618085a2ed496b33bca0f44c
Nandini Jindal <nandinijindal010@gmail.com>
2024-08-10 17:53:44 +0530

    remove elemet

--- Nandini/Striver_SDE_Sheet/TwoPointer/RemoveElement.cpp
+++ Nandini/Striver_SDE_Sheet/TwoPointer/RemoveElement.cpp
@@ -20,3 +20,16 @@ public:
        return l; 
     }
 };
+class Solution2 {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int ind = 0;
+        for(int i = 0; i< nums.size(); i++){
+            if(nums[i] != val){
+                nums[ind] = nums[i];
+                ind++;
+            }
+        }
+        return ind;
+    }
+};
