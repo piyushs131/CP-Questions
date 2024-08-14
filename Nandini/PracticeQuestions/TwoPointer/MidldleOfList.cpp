02b2a426317aa134f75a627e80476deff14fcf9e
Nandini Jindal <nandinijindal010@gmail.com>
2024-08-14 08:31:12 +0530

    middle of linked list

---
+++ Nandini/PracticeQuestions/TwoPointer/MidldleOfList.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+ 
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) 
+    {
+       ListNode* sp = head;
+       ListNode* fp = head;
+       while(fp!=NULL && fp->next!=NULL)
+       {
+        sp = sp->next;
+        fp = fp->next->next;
+       } 
+       return sp;
+    }
+};
