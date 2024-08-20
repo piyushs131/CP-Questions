f182b58809a12ff54702efdd43209d9d2695cc6d
Nandini Jindal <nandinijindal010@gmail.com>
2024-08-20 20:14:27 +0530

    make all equal

--- Nandini/Check.cpp
+++ Nandini/Check.cpp
@@ -2,29 +2,20 @@
 using namespace std;
 #define ll long long
 #define all(x) begin(x), end(x)
-void solve() {
-    ll n, k;
-    cin >> n >> k;
-    vector<ll> v(n);
-    
-    for (int i = 0; i < n; i++) 
-        cin >> v[i];
-
-    sort(v.rbegin(), v.rend());
-    ll ans =0;
-     for (int i = 0; i < n; i++) {
-       if(i&1) ans-=v[i]; 
-       else ans+=v[i];
-    }
-
-    vector<ll> b;
-    for(int i=1 ; i<n ; i+=2)
-    {
-        b.push_back(v[i-1]-v[i]);
-    }
-    
-    ans-=min(k,accumulate(all(b),0LL));
-    cout << abs(ans) << endl;
+void solve() 
+{
+    int n ;
+    cin >> n;
+    vector<int> arr(n);
+    for(int i=0 ; i<n ; i++) cin >> arr[i];
+   map<int,int> mp;
+   int ans =0;
+   for(auto it :  arr)
+   {
+     mp[it]++;
+     if(ans<mp[it]) ans = mp[it];
+   }
+   cout << n-ans << endl;
 }
 
 int main() {
