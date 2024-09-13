bf759ef55967f202188d7465ddb52d063841dad5
Nandini Jindal <nandinijindal010@gmail.com>
2024-09-13 08:16:32 +0530

    compliment

--- Nandini/PracticeQuestions/BitManupulation/Complement.cpp
+++ Nandini/PracticeQuestions/BitManupulation/Complement.cpp
@@ -4,18 +4,12 @@ class Solution {
 public:
     int bitwiseComplement(int n) 
     {
-      int bits = 1;
-      int num = n;
-      num = num/2;
-      while(num>0)
-      {
-        bits++;
-        num /=2;
-      }  
-      for(int i=0 ; i<bits ; i++)
-      {
-         n = n^(1<<i);
-      }
-      return n;
+        // eg when n = 5
+        // msb = 3(number of bits in 5) and 1<<msb is 8 
+        // 8-1 = 7 ^ 5 = 2-> ans as 1 1 1 ^ 1 0 1 = 0 1 0 
+        if(n==0) return 1;
+        // most significant bit
+        int msb = log2(n) + 1 ;
+        return ((1<<msb)-1)^n;  
     }
 };
