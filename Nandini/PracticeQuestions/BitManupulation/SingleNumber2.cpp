fd7490b604e974690d79c0a2e384581ba051c175
Nandini Jindal <nandinijindal010@gmail.com>
2024-09-13 14:55:04 +0530

    single numebr 2

--- Nandini/PracticeQuestions/BitManupulation/SingleNumber2.cpp
+++ Nandini/PracticeQuestions/BitManupulation/SingleNumber2.cpp
@@ -17,3 +17,17 @@ public:
       return ans;
     }
 };
+class Solution2 {
+public:
+    int singleNumber(vector<int>& nums) 
+    {
+       int ones =0,twos=0;
+       int n = nums.size();
+       for(int i=0 ; i<n ; i++) 
+       {
+         ones = ((ones^nums[i]) & ~twos);
+         twos = ((twos^nums[i]) & ~ones);
+       }
+       return ones;
+    }
+};
