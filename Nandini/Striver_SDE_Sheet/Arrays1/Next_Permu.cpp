ab985cce80404d35e11fecc322fd3da1482f8433
Piyush Saxena <140651227+piyushs131@users.noreply.github.com>
2024-12-24 09:58:26 +0530

    Delete Nandini directory

--- Nandini/Striver_SDE_Sheet/Arrays1/Next_Permu.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-class Solution {
-public:
-    void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());  
-    }
-};
--- Nandini/Striver_SDE_Sheet/BitManupulation/Learning/SetRightMostUnset.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-class Solution {
-  public:
-    int setBit(int n) 
-    {
-      return n | (n+1); 
-    }
-};
